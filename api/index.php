cadbb084c6b5d6990a3fc5182c30f364b857fd84
tenno <tes81925@gmail.com>
2026-05-17 13:14:58 +0700

    fix: force dynamic bypass for container binding issues on serverless runtime

--- api/index.php
+++ api/index.php
@@ -1,4 +1,15 @@
 <?php
 
-// Mengarahkan request Vercel Serverless ke public index bawaan Laravel
+// Paksa hapus penunjuk cache lama jika entah bagaimana terbaca oleh serverless runtime
+$cachedConfig = __DIR__ . '/../bootstrap/cache/config.php';
+if (file_exists($cachedConfig)) {
+    @unlink($cachedConfig);
+}
+
+$cachedServices = __DIR__ . '/../bootstrap/cache/services.php';
+if (file_exists($cachedServices)) {
+    @unlink($cachedServices);
+}
+
+// Teruskan request ke public index bawaan Laravel
 require __DIR__ . '/../public/index.php';
